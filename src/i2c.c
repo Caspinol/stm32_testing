508a68ea2749f553123bdd6bd1e2f3404fa6741f
Krzysztof <kristhesavage@gmail.com>
2015-02-22 22:31:50 +0000

    Configured system clock to use external crystal

--- src/i2c.c
+++ src/i2c.c
@@ -1,7 +1,27 @@
 #include <stm32f401xc.h>
+#include "utils.h"
 #include "i2c.h"
 
-void quad_i2c_init(){
+/*
+ *This i2c driver will be needed to communicate with the 
+ *LSM303  accelometer/magnetometer
+ *PINOUT:
+        SCL - PB6
+        SDA - PB9
+        INT1 - PE5
+        INT2 - PE4
+*/
 
+void qc_i2c_init(){
+  /* Init GPIOB clock */
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+  /* enable clock for i2c1 peripheral */
+  RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+  /* configure the GPIOB pin 6 adn 9 as AF4 */
+  GPIOB->AFR[0] |= 0x04000000;
+  GPIOB->AFR[1] |= 0x00000040;
+  /* open drain */
+  GPIOB->OTYPER |= (GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_9);
+  
 }
 
--- src/i2c.h
+++ src/i2c.h
@@ -1,6 +1,7 @@
 #ifndef __I2C_H_
 #define __I2C_H_
 
-void quad_i2c_init();
+void qc_i2c_init();
+void qc_i2c_write();
 
 #endif
